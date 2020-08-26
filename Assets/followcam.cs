97c2fc89a43ff83e196f556f1cf22bf0e4341feb
Holton Johnson <holtonk.johnson@gmail.com>
2020-08-26 10:41:57 -0500

    Built Level, Follow Cam, and Controller

---
+++ Assets/followcam.cs
@@ -0,0 +1,34 @@
+﻿//Holton Johnson
+//follow the player's x position,
+//but not anything else
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followcam : MonoBehaviour
+{
+    [Tooltip("The object you want to follow.")]
+    public Transform target;
+
+    // the starting position of the camera
+    Vector3 startPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //save the starting position of the camera
+        startPos = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // get the position of the target but keep the starting y and z positions.
+        Vector3 newPos = new Vector3(target.position.x, startPos.y, startPos.z);
+
+        // assign the new position to this camera.
+        this.transform.position = newPos;
+    }
+}
---
+++ Assets/playercontroller.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class playercontroller : MonoBehaviour
+{
+    Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Player Controller Starting Up!");
+
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Time since last frame = " + Time.deltaTime);
+
+        if(Input.anyKeyDown) Jump();
+    }
+
+//FixedUpdate is for physics, it runs 50 times / second.
+    void FixedUpdate() {
+        Debug.Log("Fixed Update frame time =" +Time.deltaTime);
+
+        // adding forward force
+        rb.AddRelativeForce(Vector3.right * 10);
+    }
+
+    void Jump() {
+        rb.AddRelativeForce(Vector3.up * 20, ForceMode.Impulse);
+    }
+}
